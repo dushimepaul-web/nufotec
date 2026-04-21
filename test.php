b4d544f434d4a73bd8060369033fabdcad6317dc
dushimepaul-web <dushimepaul51@gmail.com>
2026-04-21 15:02:58 +0200

    hduwyw

--- test.php
+++ test.php
@@ -1,8 +1,1 @@
-<?php
-// test.php - à placer à la racine du site
-echo "Test direct PHP fonctionne<br>";
-
-// Vérifier l'environnement
-echo "Document Root: " . $_SERVER['DOCUMENT_ROOT'] . "<br>";
-echo "Script Name: " . $_SERVER['SCRIPT_NAME'] . "<br>";
-echo "Request URI: " . $_SERVER['REQUEST_URI'] . "<br>";
+<?php phpinfo(); ?>
