e32f8c23e939f80160b9cb6fe00973abf5559682
dushimepaul-web <dushimepaul51@gmail.com>
2026-05-07 20:18:45 +0200

    update

--- application/config/development/database.php
+++ application/config/development/database.php
@@ -1,58 +1,74 @@
+```php
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $active_group = 'default';
 $query_builder = TRUE;
 
-// Détection automatique environnement
-if ($_SERVER['SERVER_NAME'] == 'localhost') {
+/*
+|--------------------------------------------------------------------------
+| LOCALHOST WINDOWS
+|--------------------------------------------------------------------------
+*/
+if (
+    isset($_SERVER['SERVER_NAME']) &&
+    (
+        $_SERVER['SERVER_NAME'] == 'localhost' ||
+        $_SERVER['SERVER_NAME'] == '127.0.0.1'
+    )
+) {
 
-    // Localhost Windows
     $db['default'] = array(
-        'dsn'       => '',
-        'hostname'  => 'localhost',
-        'username'  => 'root',
-        'password'  => '',
-        'database'  => 'nufotec_db',
-        'dbdriver'  => 'mysqli',
-        'dbprefix'  => '',
-        'pconnect'  => FALSE,
-        'db_debug'  => (ENVIRONMENT !== 'production'),
-        'cache_on'  => FALSE,
-        'cachedir'  => '',
-        'char_set'  => 'utf8',
-        'dbcollat'  => 'utf8_general_ci',
-        'swap_pre'  => '',
-        'encrypt'   => FALSE,
-        'compress'  => FALSE,
-        'stricton'  => FALSE,
-        'failover'  => array(),
-        'save_queries' => TRUE
+        'dsn'           => '',
+        'hostname'      => 'localhost',
+        'username'      => 'root',
+        'password'      => '',
+        'database'      => 'nufotec_db',
+        'dbdriver'      => 'mysqli',
+        'dbprefix'      => '',
+        'pconnect'      => FALSE,
+        'db_debug'      => (ENVIRONMENT !== 'production'),
+        'cache_on'      => FALSE,
+        'cachedir'      => '',
+        'char_set'      => 'utf8',
+        'dbcollat'      => 'utf8_general_ci',
+        'swap_pre'      => '',
+        'encrypt'       => FALSE,
+        'compress'      => FALSE,
+        'stricton'      => FALSE,
+        'failover'      => array(),
+        'save_queries'  => TRUE
     );
 
 } else {
 
-    // VPS Docker
+    /*
+    |--------------------------------------------------------------------------
+    | VPS DOCKER
+    |--------------------------------------------------------------------------
+    */
+
     $db['default'] = array(
-        'dsn'       => '',
-        'hostname'  => 'db',
-        'username'  => 'root',
-        'password'  => 'root123',
-        'database'  => 'nufotec_db',
-        'dbdriver'  => 'mysqli',
-        'dbprefix'  => '',
-        'pconnect'  => FALSE,
-        'db_debug'  => (ENVIRONMENT !== 'production'),
-        'cache_on'  => FALSE,
-        'cachedir'  => '',
-        'char_set'  => 'utf8',
-        'dbcollat'  => 'utf8_general_ci',
-        'swap_pre'  => '',
-        'encrypt'   => FALSE,
-        'compress'  => FALSE,
-        'stricton'  => FALSE,
-        'failover'  => array(),
-        'save_queries' => TRUE
+        'dsn'           => '',
+        'hostname'      => 'db',
+        'username'      => 'root',
+        'password'      => 'root123',
+        'database'      => 'nufotec_db',
+        'dbdriver'      => 'mysqli',
+        'dbprefix'      => '',
+        'pconnect'      => FALSE,
+        'db_debug'      => (ENVIRONMENT !== 'production'),
+        'cache_on'      => FALSE,
+        'cachedir'      => '',
+        'char_set'      => 'utf8',
+        'dbcollat'      => 'utf8_general_ci',
+        'swap_pre'      => '',
+        'encrypt'       => FALSE,
+        'compress'      => FALSE,
+        'stricton'      => FALSE,
+        'failover'      => array(),
+        'save_queries'  => TRUE
     );
 
 }
+```
