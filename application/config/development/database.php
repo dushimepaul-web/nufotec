5ae89b201ae50773716ba808ef6812f4e95ff39d
dushimepaul-web <dushimepaul51@gmail.com>
2026-05-10 00:03:17 +0200

    update

--- application/config/development/database.php
+++ application/config/development/database.php
@@ -4,76 +4,51 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $active_group = 'default';
 $query_builder = TRUE;
 
-/*
-|--------------------------------------------------------------------------
-| CONFIGURATION BASE DE DONNÉES
-|--------------------------------------------------------------------------
-*/
-
-if (
-    isset($_SERVER['SERVER_NAME']) &&
-    (
-        $_SERVER['SERVER_NAME'] === 'localhost' ||
-        $_SERVER['SERVER_NAME'] === '127.0.0.1'
-    )
-) {
-
-    /*
-    |--------------------------------------------------------------------------
-    | WINDOWS LOCAL
-    |--------------------------------------------------------------------------
-    */
-
+// Détection automatique de l'environnement
+if ($_SERVER['SERVER_NAME'] == 'localhost') {
+    // Configuration pour le serveur local
     $db['default'] = array(
-        'dsn'           => '',
-        'hostname'      => 'localhost',
-        'username'      => 'root',
-        'password'      => '',
-        'database'      => 'nufotec_db',
-        'dbdriver'      => 'mysqli',
-        'dbprefix'      => '',
-        'pconnect'      => FALSE,
-        'db_debug'      => (ENVIRONMENT !== 'production'),
-        'cache_on'      => FALSE,
-        'cachedir'      => '',
-        'char_set'      => 'utf8',
-        'dbcollat'      => 'utf8_general_ci',
-        'swap_pre'      => '',
-        'encrypt'       => FALSE,
-        'compress'      => FALSE,
-        'stricton'      => FALSE,
-        'failover'      => array(),
-        'save_queries'  => TRUE
+        'dsn'       => '',
+        'hostname'  => 'localhost',
+        'username'  => 'root',
+        'password'  => '',          // mot de passe local
+        'database'  => 'nufotec_db',
+        'dbdriver'  => 'mysqli',
+        'dbprefix'  => '',
+        'pconnect'  => FALSE,
+        'db_debug'  => (ENVIRONMENT !== 'production'),
+        'cache_on'  => FALSE,
+        'cachedir'  => '',
+        'char_set'  => 'utf8',
+        'dbcollat'  => 'utf8_general_ci',
+        'swap_pre'  => '',
+        'encrypt'   => FALSE,
+        'compress'  => FALSE,
+        'stricton'  => FALSE,
+        'failover'  => array(),
+        'save_queries' => TRUE
     );
-
 } else {
-
-    /*
-    |--------------------------------------------------------------------------
-    | VPS DOCKER
-    |--------------------------------------------------------------------------
-    */
-
+    // Configuration pour le serveur distant
     $db['default'] = array(
-        'dsn'           => '',
-        'hostname'      => 'db',
-        'username'      => 'root',
-        'password'      => 'root123',
-        'database'      => 'nufotec_db',
-        'dbdriver'      => 'mysqli',
-        'dbprefix'      => '',
-        'pconnect'      => FALSE,
-        'db_debug'      => (ENVIRONMENT !== 'production'),
-        'cache_on'      => FALSE,
-        'cachedir'      => '',
-        'char_set'      => 'utf8',
-        'dbcollat'      => 'utf8_general_ci',
-        'swap_pre'      => '',
-        'encrypt'       => FALSE,
-        'compress'      => FALSE,
-        'stricton'      => FALSE,
-        'failover'      => array(),
-        'save_queries'  => TRUE
+        'dsn'       => '',
+        'hostname'  => 'localhost',
+        'username'  => 'nufotec_nufotec',
+        'password'  => 'Paul@2026', // mot de passe distant
+        'database'  => 'nufotec_db',
+        'dbdriver'  => 'mysqli',
+        'dbprefix'  => '',
+        'pconnect'  => FALSE,
+        'db_debug'  => (ENVIRONMENT !== 'production'),
+        'cache_on'  => FALSE,
+        'cachedir'  => '',
+        'char_set'  => 'utf8',
+        'dbcollat'  => 'utf8_general_ci',
+        'swap_pre'  => '',
+        'encrypt'   => FALSE,
+        'compress'  => FALSE,
+        'stricton'  => FALSE,
+        'failover'  => array(),
+        'save_queries' => TRUE
     );
-
 }
