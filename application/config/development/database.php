2a35322765a432f181dec638d902014b6b7f5444
dushimepaul-web <dushimepaul51@gmail.com>
2026-05-11 11:25:28 +0200

    hh

--- application/config/development/database.php
+++ application/config/development/database.php
@@ -8,25 +8,26 @@ $query_builder = TRUE;
 if ($_SERVER['SERVER_NAME'] == 'localhost') {
     // Configuration pour le serveur local
     $db['default'] = array(
-        'dsn'       => '',
-        'hostname'  => 'localhost',
-        'username'  => 'root',
-        'password'  => '',          // mot de passe local
-        'database'  => 'nufotec_db',
-        'dbdriver'  => 'mysqli',
-        'dbprefix'  => '',
-        'pconnect'  => FALSE,
-        'db_debug'  => (ENVIRONMENT !== 'production'),
-        'cache_on'  => FALSE,
-        'cachedir'  => '',
-        'char_set'  => 'utf8',
-        'dbcollat'  => 'utf8_general_ci',
-        'swap_pre'  => '',
-        'encrypt'   => FALSE,
-        'compress'  => FALSE,
-        'stricton'  => FALSE,
-        'failover'  => array(),
-        'save_queries' => TRUE
+    'dsn'       => '',
+    'hostname'  => 'localhost',
+    'username'  => 'nufotec_nufotec',
+    'password'  => 'root123',
+    'database'  => 'nufotec_db',
+    'dbdriver'  => 'mysqli',
+    'dbprefix'  => '',
+    'pconnect'  => FALSE,
+    'db_debug'  => (ENVIRONMENT !== 'production'),
+    'cache_on'  => FALSE,
+    'cachedir'  => '',
+    'char_set'  => 'utf8mb4',
+    'dbcollat'  => 'utf8mb4_general_ci',
+    'swap_pre'  => '',
+    'encrypt'   => FALSE,
+    'compress'  => FALSE,
+    'stricton'  => FALSE,
+    'failover'  => array(),
+    'save_queries' => TRUE
+
     );
 } else {
     // Configuration pour le serveur distant
