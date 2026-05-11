ce118cead374ff74a7cd2163d492d3a94f4a11a7
dushimepaul-web <dushimepaul51@gmail.com>
2026-05-11 14:54:46 +0200

    hh

--- application/config/development/autoload.php
+++ application/config/development/autoload.php
@@ -89,7 +89,7 @@ $autoload['drivers'] = array();
 |
 |	$autoload['helper'] = array('url', 'file');
 */
-$autoload['helper'] = array('url', 'form', 'cookie', 'zapier','file','twilio','whapi','security','role','lang');
+$autoload['helper'] = array('url', 'form', 'cookie', 'zapier','file','twilio','whapi','security','role','lang','MY_url');
 
 /*
 | -------------------------------------------------------------------
---
+++ application/helpers/MY_url_helper.php
@@ -0,0 +1,13 @@
+<?php
+if (!function_exists('base_url')) {
+    function base_url($uri = '')
+    {
+        $CI =& get_instance();
+        
+        // Enlever le préfixe langue (fr, en, sw) du lien
+        $uri = preg_replace('#^(fr|en|sw)/#', '', $uri);
+        $uri = preg_replace('#^(fr|en|sw)$#', '', $uri);
+        
+        return $CI->config->slash_item('base_url') . $uri;
+    }
+}
